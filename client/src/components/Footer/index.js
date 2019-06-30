6667e42a1e89e1f82cfd738475154c1d89cc44e1
G. Pierce Lusk <gpiercelusk@gmail.com>
2019-06-30 19:55:09 -0400

    wireframe of components up basic css added

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <hr />
+      <p className="pull-right">
+        <i className="fab fa-github" /> G Pierce Lusk 2019
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -26,7 +26,7 @@ class Nav extends Component {
     window.addEventListener("resize", this.updateWidth);
   }
 
-  // // componentWillUnMount() {
+  // componentWillUnMount() {
   //   window.removeEventListener("resize", this.updateWidth);
   // }
 
