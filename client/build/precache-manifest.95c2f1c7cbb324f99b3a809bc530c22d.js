1e6c212e56eccb6e40ee3bea59e61d85c51aec00
G. Pierce Lusk <gpiercelusk@gmail.com>
2019-07-01 18:36:07 -0400

    build folder added

---
+++ client/build/precache-manifest.95c2f1c7cbb324f99b3a809bc530c22d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "43442d8e42289be538a5",
+    "url": "/static/css/main.cb5f854b.chunk.css"
+  },
+  {
+    "revision": "43442d8e42289be538a5",
+    "url": "/static/js/main.aeb6c649.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b3caa7daac95209a0fd7",
+    "url": "/static/js/2.e76eeebc.chunk.js"
+  },
+  {
+    "revision": "4d8575e4fbd51818260f3efee022d08e",
+    "url": "/index.html"
+  }
+];
